cc88fc77819167cb86a6b1b0dcc14794173bf74b
MD_SHAHAB <137146198+Shahab-16@users.noreply.github.com>
2024-11-11 17:58:04 +0530

    Update StoreContext.js

--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -9,7 +9,7 @@ const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems]=useState({});
     const [login,setlogin]=useState(false);
-    const url='http://localhost:5000';
+    const url='https://yumzy-backend.onrender.com';
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
 
